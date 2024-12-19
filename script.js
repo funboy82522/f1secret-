4cde0005c418a26243f753c966e78656ccfc8c87
funboy82522 <thestevensons@gmail.com>
2024-12-18 19:22:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// script.js
 
+function openGame(event, url) {
+    event.preventDefault();  // Prevent default anchor behavior
+    window.open(url, '_blank');  // Open the game link in a new tab
+    setTimeout(() => {
+        window.open('about:blank', '_blank');  // Open a blank page
+    }, 100);  // Small delay to ensure the previous action executes
+}
+
+function searchGames() {
+    const input = document.getElementById('searchBar').value.toLowerCase(); // Get the input value
+    const gameCards = document.querySelectorAll('.game-card'); // Get all game cards
+
+    gameCards.forEach(card => {
+        const title = card.querySelector('.button-title').textContent.toLowerCase(); // Get the title of the game
+        if (title.includes(input)) {
+            card.style.display = ''; // Show the game card
+        } else {
+            card.style.display = 'none'; // Hide the game card
+        }
+    });
+}
